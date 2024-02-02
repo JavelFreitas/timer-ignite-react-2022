abb216e281e4612c1e48accfcbb83bda4c42abd2
Javel Queiroz <javelqueiroz@outlook.com.br>
2024-02-01 22:14:27 -0300

    chore: add page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Hello</div>
+      <BrowserRouter>
+        <Router />
 
-      <GlobalStyle />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
